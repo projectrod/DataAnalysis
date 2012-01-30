95ff504d52d2ed2f4224ca4882243d6da447d8cf
Leonard Punt <leonardpunt@gmail.com>
2012-01-30 15:52:32 +0100

    finished mapper

--- src/dataanalysis/MapValuesByDate.java
+++ src/dataanalysis/MapValuesByDate.java
@@ -1,10 +1,8 @@
 package dataanalysis;
 
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.client.Put;
@@ -15,32 +13,41 @@ import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
 import org.apache.hadoop.hbase.mapreduce.TableMapper;
 import org.apache.hadoop.hbase.mapreduce.TableReducer;
 import org.apache.hadoop.hbase.util.Bytes;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper.Context;
 
 public class MapValuesByDate {
 
-    public static class DateMapper extends TableMapper<ImmutableBytesWritable, IntWritable> {
+    public static class DateMapper extends TableMapper<ImmutableBytesWritable, ImmutableBytesWritable> {
         
-        public void map(ImmutableBytesWritable key, Result value, Context context) throws IOException, InterruptedException {
+        public void map(ImmutableBytesWritable row, Result value, Context context) throws IOException, InterruptedException {
             String dateTime = Bytes.toString(value.getValue(Bytes.toBytes("attributes"), Bytes.toBytes("Date")));
+            byte[] badge = value.getValue(Bytes.toBytes("attributes"), Bytes.toBytes("Name"));
             String date = dateTime.split("T")[0];
-			context.write(new ImmutableBytesWritable(Bytes.toBytes(date)), new IntWritable(1));
+			context.write(new ImmutableBytesWritable(Bytes.toBytes(date)), new ImmutableBytesWritable(badge));
         }
         
     }
     
-    public static class DateReducer extends TableReducer<ImmutableBytesWritable, IntWritable, ImmutableBytesWritable> {
+    public static class DateReducer extends TableReducer<ImmutableBytesWritable, ImmutableBytesWritable, ImmutableBytesWritable> {
 
-    	public void reduce(ImmutableBytesWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-    		int i = 0;
-    		for (IntWritable val : values) {
-    			i += val.get();
+    	public void reduce(ImmutableBytesWritable row, Iterable<ImmutableBytesWritable> values, Context context) throws IOException, InterruptedException {
+    		Map<String, Integer> badgesCount = new HashMap<String, Integer>();
+    		for (ImmutableBytesWritable val : values) {
+    			String key = Bytes.toString(val.get());
+    			if (badgesCount.containsKey(key)) {
+    				badgesCount.put(key, badgesCount.get(key) + 1);
+    			}
+    			else {
+    				badgesCount.put(key, 1);
+    			}
     		}
-    		Put put = new Put(Bytes.toBytes(key.toString()));
-    		put.add(Bytes.toBytes("t1"), Bytes.toBytes("count"), Bytes.toBytes(i));
-
-    		context.write(null, put);
+    		
+    		Put put = new Put(row.get());
+    		for (String key : badgesCount.keySet()) {
+    			put.add(Bytes.toBytes("t1"), Bytes.toBytes(key), Bytes.toBytes(String.valueOf(badgesCount.get(key))));
+    		}
+    		context.write(new ImmutableBytesWritable(row.get()), put);
     	}
     }
 
@@ -50,8 +57,9 @@ public class MapValuesByDate {
         job.setJarByClass(MapValuesByDate.class);
         Scan scan = new Scan();
         scan.addColumn(Bytes.toBytes("attributes"), Bytes.toBytes("Date"));
+        scan.addColumn(Bytes.toBytes("attributes"), Bytes.toBytes("Name"));
         TableMapReduceUtil.initTableMapperJob("badges", scan, DateMapper.class, 
-        		ImmutableBytesWritable.class, IntWritable.class, job);
+        		ImmutableBytesWritable.class, ImmutableBytesWritable.class, job);
         TableMapReduceUtil.initTableReducerJob("test", DateReducer.class, job);
         job.setNumReduceTasks(1);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
