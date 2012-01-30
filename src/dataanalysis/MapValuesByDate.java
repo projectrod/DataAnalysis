1348736e48a9aec2a6989303f1affe4958b09afd
Albert Groothedde <albertgroothedde@gmail.com>
2012-01-30 16:21:18 +0100

    Added /src/lib to .gitignore. Completed MapReduce MapValuesByData

--- src/dataanalysis/MapValuesByDate.java
+++ src/dataanalysis/MapValuesByDate.java
@@ -19,36 +19,34 @@ import org.apache.hadoop.mapreduce.Mapper.Context;
 public class MapValuesByDate {
 
     public static class DateMapper extends TableMapper<ImmutableBytesWritable, ImmutableBytesWritable> {
-        
+
         public void map(ImmutableBytesWritable row, Result value, Context context) throws IOException, InterruptedException {
             String dateTime = Bytes.toString(value.getValue(Bytes.toBytes("attributes"), Bytes.toBytes("Date")));
             byte[] badge = value.getValue(Bytes.toBytes("attributes"), Bytes.toBytes("Name"));
             String date = dateTime.split("T")[0];
-			context.write(new ImmutableBytesWritable(Bytes.toBytes(date)), new ImmutableBytesWritable(badge));
+            context.write(new ImmutableBytesWritable(Bytes.toBytes(date)), new ImmutableBytesWritable(badge));
         }
-        
     }
-    
+
     public static class DateReducer extends TableReducer<ImmutableBytesWritable, ImmutableBytesWritable, ImmutableBytesWritable> {
 
-    	public void reduce(ImmutableBytesWritable row, Iterable<ImmutableBytesWritable> values, Context context) throws IOException, InterruptedException {
-    		Map<String, Integer> badgesCount = new HashMap<String, Integer>();
-    		for (ImmutableBytesWritable val : values) {
-    			String key = Bytes.toString(val.get());
-    			if (badgesCount.containsKey(key)) {
-    				badgesCount.put(key, badgesCount.get(key) + 1);
-    			}
-    			else {
-    				badgesCount.put(key, 1);
-    			}
-    		}
-    		
-    		Put put = new Put(row.get());
-    		for (String key : badgesCount.keySet()) {
-    			put.add(Bytes.toBytes("t1"), Bytes.toBytes(key), Bytes.toBytes(String.valueOf(badgesCount.get(key))));
-    		}
-    		context.write(new ImmutableBytesWritable(row.get()), put);
-    	}
+        public void reduce(ImmutableBytesWritable row, Iterable<ImmutableBytesWritable> values, Context context) throws IOException, InterruptedException {
+            Map<String, Integer> badgesCount = new HashMap<String, Integer>();
+            for (ImmutableBytesWritable val : values) {
+                String key = Bytes.toString(val.get());
+                if (badgesCount.containsKey(key)) {
+                    badgesCount.put(key, badgesCount.get(key) + 1);
+                } else {
+                    badgesCount.put(key, 1);
+                }
+            }
+
+            Put put = new Put(row.get());
+            for (String key : badgesCount.keySet()) {
+                put.add(Bytes.toBytes("t1"), Bytes.toBytes(key), Bytes.toBytes(String.valueOf(badgesCount.get(key))));
+            }
+            context.write(new ImmutableBytesWritable(row.get()), put);
+        }
     }
 
     public static void main(String args[]) throws Exception {
@@ -58,9 +56,9 @@ public class MapValuesByDate {
         Scan scan = new Scan();
         scan.addColumn(Bytes.toBytes("attributes"), Bytes.toBytes("Date"));
         scan.addColumn(Bytes.toBytes("attributes"), Bytes.toBytes("Name"));
-        TableMapReduceUtil.initTableMapperJob("badges", scan, DateMapper.class, 
-        		ImmutableBytesWritable.class, ImmutableBytesWritable.class, job);
-        TableMapReduceUtil.initTableReducerJob("test", DateReducer.class, job);
+        TableMapReduceUtil.initTableMapperJob("badges", scan, DateMapper.class,
+                ImmutableBytesWritable.class, ImmutableBytesWritable.class, job);
+        TableMapReduceUtil.initTableReducerJob("dates_badges", DateReducer.class, job);
         job.setNumReduceTasks(1);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
